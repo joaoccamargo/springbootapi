f64192bc886d22137b1dc44ed8c6bd12759328fa
João Camargo <jclaudio00_12@hotmail.com>
2023-06-01 14:57:49 -0300

    initial commit

---
+++ src/test/java/io/github/joaoccamargo/springbootapi/SpringbootapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaoccamargo.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
