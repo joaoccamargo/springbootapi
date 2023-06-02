037c90214d132230e8ecb2b60eb2b5844cc582ac
João Camargo <jclaudio00_12@hotmail.com>
2023-06-02 12:54:20 -0300

    Pedido configurado lista de pedidos

--- src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/Pedido.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/Pedido.java
@@ -2,6 +2,7 @@ package io.github.joaoccamargo.springbootapi.domain.entity;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +11,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,31 +34,48 @@ public class Pedido {
     // 10000.00
     @Column(name = "total", length = 20, precision = 2)
     private BigDecimal total;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
     
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+    
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
+
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public Cliente getCliente() {
         return cliente;
     }
+
     public void setCliente(Cliente cliente) {
         this.cliente = cliente;
     }
+
     public LocalDate getDataPedido() {
         return dataPedido;
     }
+
     public void setDataPedido(LocalDate dataPedido) {
         this.dataPedido = dataPedido;
     }
+
     public BigDecimal getTotal() {
         return total;
     }
+
     public void setTotal(BigDecimal total) {
         this.total = total;
     }
-    
 
 }
