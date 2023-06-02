ff553e68053cf15d82c28396b72c2a80757796a1
João Camargo <jclaudio00_12@hotmail.com>
2023-06-02 12:06:23 -0300

    produto configurado

--- src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/Produto.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/Produto.java
@@ -2,10 +2,26 @@ package io.github.joaoccamargo.springbootapi.domain.entity;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "descricao", length = 250)
     private String descricao;
+
+    @Column(name = "preco_unitario")    
     private BigDecimal preco;
     
     public Integer getId() {
