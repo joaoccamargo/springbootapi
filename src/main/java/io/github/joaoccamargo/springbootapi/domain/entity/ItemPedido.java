d433e878f637abf637190157772f5fadc4f2fb15
João Camargo <jclaudio00_12@hotmail.com>
2023-06-02 12:49:27 -0300

    item pedido configurado

--- src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/ItemPedido.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/ItemPedido.java
@@ -1,10 +1,32 @@
 package io.github.joaoccamargo.springbootapi.domain.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "item_pedido")
 public class ItemPedido {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
     private Pedido pedido;
+    
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
     private Produto produto;
+
+    @Column(name = "quantidade")
     private Integer quantidade;
     
     public Integer getId() {
