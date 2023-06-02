cea9295323d160086298d9ba916aae6c706a5d81
João Camargo <jclaudio00_12@hotmail.com>
2023-06-02 11:42:32 -0300

    query method modifying

--- src/main/java/io/github/joaoccamargo/springbootapi/domain/repository/Clientes.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/domain/repository/Clientes.java
@@ -1,16 +1,26 @@
 package io.github.joaoccamargo.springbootapi.domain.repository;
 
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import io.github.joaoccamargo.springbootapi.domain.entity.Cliente;
 
 public interface Clientes extends JpaRepository<Cliente, Integer>{
 
     // Transforma metodo em query, query methods
-    List<Cliente> findByNomeLike(String nome);
-    //List<Cliente> findByNomeOrIdOrderById(String nome, Integer id);
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true) // consulta nativa sql
+    //@Query(value = " select c from Cliente c where c.nome like :nome ") // consulta jpql
+    List<Cliente> encontrarPorNome( @Param("nome") String nome);
+
+    @Query(" delete from Cliente c where c.nome =:nome ")
+    @Modifying
+    void deleteByNome(String nome);
     
-    boolean existsByNome(String nome);
+    
+    //List<Cliente> findByNomeOrIdOrderById(String nome, Integer id);
+    //boolean existsByNome(String nome);
 }
