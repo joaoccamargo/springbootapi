604b3b22cac7aeea1e7b5cadcb2bdeadd89ee1e8
João Camargo <jclaudio00_12@hotmail.com>
2023-06-05 13:54:43 -0300

    controller post

--- src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/ClienteController.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/ClienteController.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.github.joaoccamargo.springbootapi.domain.entity.Cliente;
@@ -30,6 +32,13 @@ public class ClienteController {
         }
 
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping("/api/clientes")
+    @ResponseBody
+    public ResponseEntity save(@RequestBody Cliente cliente){
+        Cliente clienteSalvo = clientes.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
     }
     
 }
