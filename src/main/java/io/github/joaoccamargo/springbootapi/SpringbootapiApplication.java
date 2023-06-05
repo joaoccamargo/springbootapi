5078d87232a469f28e4ddbcd56023683c7bf839b
João Camargo <jclaudio00_12@hotmail.com>
2023-06-05 13:53:46 -0300

    removed bean command

--- src/main/java/io/github/joaoccamargo/springbootapi/SpringbootapiApplication.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/SpringbootapiApplication.java
@@ -1,24 +1,10 @@
 package io.github.joaoccamargo.springbootapi;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.joaoccamargo.springbootapi.domain.entity.Cliente;
-import io.github.joaoccamargo.springbootapi.domain.repository.Clientes;
 
 @SpringBootApplication
 public class SpringbootapiApplication {
-
-	@Bean
-	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
-		return args -> {
-			Cliente c = new Cliente(null, "fulano");
-			clientes.save(c);
-		};
-	}
 
 
 	public static void main(String[] args) {
