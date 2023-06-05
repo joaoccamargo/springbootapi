505c607a08e2168b34c5c9e451b08226679846f1
João Camargo <jclaudio00_12@hotmail.com>
2023-06-05 12:29:40 -0300

    clean get cliente id

--- src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/ClienteController.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/ClienteController.java
@@ -1,20 +1,35 @@
 package io.github.joaoccamargo.springbootapi.rest.controller;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import io.github.joaoccamargo.springbootapi.domain.entity.Cliente;
+import io.github.joaoccamargo.springbootapi.domain.repository.Clientes;
+
 @Controller
-@RequestMapping("/api/clientes")
 public class ClienteController {
 
-    
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
     @ResponseBody
-    public String helloClientes(@PathVariable("nome") String nomeCliente){
-        return String.format("Hello %s", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
+
+        return ResponseEntity.notFound().build();
     }
     
 }
