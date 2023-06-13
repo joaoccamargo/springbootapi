c256a4d9f58736fe2bedd2723f730a05f3d0aed7
João Camargo <jclaudio00_12@hotmail.com>
2023-06-13 16:11:04 -0300

    filter added

--- src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/ClienteController.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/ClienteController.java
@@ -1,7 +1,10 @@
 package io.github.joaoccamargo.springbootapi.rest.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -68,5 +71,19 @@ public class ClienteController {
             return ResponseEntity.noContent().build();
         }).orElseGet(() -> ResponseEntity.notFound().build());
     }
-    
+
+    @GetMapping("/api/clientes")
+    public ResponseEntity find(Cliente filtro){
+        ExampleMatcher matcher = ExampleMatcher
+                                    .matching()
+                                    .withIgnoreCase()
+                                    .withStringMatcher(
+                                        ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> lista = clientes.findAll(example);
+        return ResponseEntity.ok(lista);
+
+    }
+
 }
