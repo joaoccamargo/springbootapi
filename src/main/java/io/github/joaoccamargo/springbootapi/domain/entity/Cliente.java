420219635471ffb860d4b20ce50845d46e131d82
João Camargo <jclaudio00_12@hotmail.com>
2023-06-15 16:38:33 -0300

    lombok refactored client

--- src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/Cliente.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/domain/entity/Cliente.java
@@ -9,6 +9,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
@@ -16,10 +19,13 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Column;
 
+//Lombok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity // Dizendo para JPA que está é uma entidade
 @Table (name = "cliente") // Tabela do banco de dados é cliente.
 public class Cliente {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO) // Gerar ID Automaticamente H2
@@ -37,49 +43,9 @@ public class Cliente {
     @OneToMany( mappedBy = "cliente", fetch = FetchType.LAZY)
     private Set<Pedido> pedidos;
 
-    public Cliente(){}
-
     public Cliente(Integer id, String nome){
         this.id = id;
         this.nome = nome;
-    }
-
-    public Integer getId(){
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public Set<Pedido> getPedidos() {
-        return pedidos;
-    }
-
-    public void setPedidos(Set<Pedido> pedidos) {
-        this.pedidos = pedidos;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-
-    @Override
-    public String toString() {
-        return "Cliente [id=" + id + ", nome=" + nome + "]";
     }
 
    
