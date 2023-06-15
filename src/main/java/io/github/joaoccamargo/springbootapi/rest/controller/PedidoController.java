f036762b94a92c7683e61295b4f9893853352323
João Camargo <jclaudio00_12@hotmail.com>
2023-06-15 17:25:25 -0300

    post pedidocontroller

--- src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/PedidoController.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/rest/controller/PedidoController.java
@@ -1,8 +1,14 @@
 package io.github.joaoccamargo.springbootapi.rest.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.joaoccamargo.springbootapi.domain.entity.Pedido;
+import io.github.joaoccamargo.springbootapi.rest.dto.PedidoDTO;
 import io.github.joaoccamargo.springbootapi.service.PedidoService;
 
 @RestController
@@ -13,6 +19,13 @@ public class PedidoController {
 
     public PedidoController(PedidoService pedidoService){
         this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Integer save(@RequestBody PedidoDTO pedidoDTO){
+       Pedido pedido = pedidoService.salvar(pedidoDTO);
+       return pedido.getId();
     }
     
 }
