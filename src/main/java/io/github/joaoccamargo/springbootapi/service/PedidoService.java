37c34ee9e85a2b68c485e6a5bf71b6e8d7ace8f9
João Camargo <jclaudio00_12@hotmail.com>
2023-06-15 17:26:18 -0300

    pedidoservice salvar

--- src/main/java/io/github/joaoccamargo/springbootapi/service/PedidoService.java
+++ src/main/java/io/github/joaoccamargo/springbootapi/service/PedidoService.java
@@ -1,6 +1,10 @@
 package io.github.joaoccamargo.springbootapi.service;
 
+import io.github.joaoccamargo.springbootapi.domain.entity.Pedido;
+import io.github.joaoccamargo.springbootapi.rest.dto.PedidoDTO;
+
 public interface PedidoService {
+    Pedido salvar(PedidoDTO pedidoDTO);
 
     
 }
